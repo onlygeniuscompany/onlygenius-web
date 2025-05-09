9afd70b9af57dba7e51bf832a57e027a8e32e883
Ivan Serralta <serralta.ivan@gmail.com>
2025-05-09 10:11:02 -0300

    pagina de servicios

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -7,14 +7,11 @@ import PerformanceAccessModal from "../components/Services/InfoModal";
 
 export default function Services(){
     return(
-        <div className="bg-[radial-gradient(circle_at_20%_30%,#2e2e2e_0%,transparent_40%),radial-gradient(circle_at_70%_60%,#1f1f1f_0%,transparent_50%),radial-gradient(circle_at_50%_50%,#3a3a3a_0%,#0a0a0a_50%)]  text-[#FAFAFA] font-inter w-full overflow-x-hidden min-h-screen">
-            <div className="max-w-[1280px] mx-auto px-4 w-[90%]">
+        <div>
                 <Hero Modal={PerformanceAccessModal}/>
-
                 <RealAccountManagement/>
                 <CustomBotDevelopment/>
                 <Conversion/>
-            </div>
         </div>
         )
 }
