e20fcc0466deaa67f09e7bb3b5141074617ed7e3
satoshinakamoto77 <101404629+satoshinakamoto77@users.noreply.github.com>
2025-05-29 18:42:33 +0200

    Update ContactForm.jsx

--- src/components/AboutUs/ContactForm.jsx
+++ src/components/AboutUs/ContactForm.jsx
@@ -32,7 +32,7 @@ export default function ContactForm() {
       <div className="max-w-xl mx-auto">
         <div className="text-center mb-10">
           <p className="text-blue-500">Contacto</p>
-          <h2 className="text-3xl font-bold">Ponete en contacto</h2>
+          <h2 className="text-3xl font-bold">Ponte en contacto</h2>
           <p className="text-white/70 mt-2">
             Nos encantaría saber de ti. Completa este formulario.
           </p>
