7ac7ebddbea310e4c9cb90aced7a07617f749aa7
Ivan Serralta <serralta.ivan@gmail.com>
2025-06-19 11:18:26 -0300

    hero vps

---
+++ src/components/vps/Hero.jsx
@@ -0,0 +1,18 @@
+export function Hero(){
+    return(    
+        <section className="w-full  text-white py-24 px-6 md:px-12">
+
+            <div className="max-w-6xl mx-auto">
+                {/* Intro */}
+                <p className="text-sm text-blue-500 font-medium">Desde 2023</p>
+                <h2 className="text-3xl md:text-5xl font-bold mt-2 mb-4">
+                Servidores Privados Virtuales (VPS)
+                </h2>
+                <p className="text-gray-400 max-w-2xl mb-16">
+                OnlyGenius ofrece soluciones completas para inversores individuales, empresas y gestores de capital de terceros
+                que buscan rendimiento real de forma totalmente automátizada.
+                </p>
+            </div>
+        </section>
+        )
+    }
