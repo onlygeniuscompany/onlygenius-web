648c30078a49341628828076019bd07c143b6828
Ivan Serralta <serralta.ivan@gmail.com>
2025-06-20 16:13:21 -0300

    feat: make white spot background mobile friendly

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,21 +7,22 @@ export default function Layout({ children }) {
     return (
       <div className="relative text-[#FAFAFA] font-inter w-full min-h-screen overflow-x-hidden">
         
-        {/* Fondo fijo con manchas grises */}
+        {/* Fondo fijo con manchas blancas visible en desktop y mobile */}
         <div
           className="absolute inset-0 -z-10 pointer-events-none"
           style={{
             backgroundImage: `
-              radial-gradient(circle at 20% 3%,rgba(163, 163, 163, 0.12) 1%, transparent 10%),
-              radial-gradient(circle at 40% 3%,rgba(163, 163, 163, 0.12) 1%, transparent 10%),
-              radial-gradient(circle at 20% 10%, rgba(163, 163, 163, 0.12) 1%, transparent 20%),
-              radial-gradient(circle at 60% 40%, rgba(163, 163, 163, 0.12) 1%, transparent 30%),
-              radial-gradient(circle at 40% 60%, rgba(163, 163, 163, 0.12) 1%, transparent 20%),
-              radial-gradient(circle at 90% 80%, rgba(163, 163, 163, 0.12) 1%, transparent 25%),
-              radial-gradient(circle at 60% 90%, rgba(163, 163, 163, 0.3) 1%, transparent 30%)
+              radial-gradient(circle at 20% 3%,rgba(255, 255, 255, 0.12) 1%, transparent 10%),
+              radial-gradient(circle at 40% 3%,rgba(255, 255, 255, 0.12) 1%, transparent 10%),
+              radial-gradient(circle at 20% 10%, rgba(255, 255, 255, 0.12) 1%, transparent 20%),
+              radial-gradient(circle at 60% 40%, rgba(255, 255, 255, 0.12) 1%, transparent 30%),
+              radial-gradient(circle at 40% 60%, rgba(255, 255, 255, 0.12) 1%, transparent 20%),
+              radial-gradient(circle at 90% 80%, rgba(255, 255, 255, 0.12) 1%, transparent 25%),
+              radial-gradient(circle at 60% 90%, rgba(255, 255, 255, 0.3) 1%, transparent 30%)
             `,
             backgroundColor: "#09090B",
-            backgroundRepeat: "no-repeat",
+            backgroundRepeat: "repeat",
+            backgroundAttachment: "fixed",
             backgroundSize: "cover"
           }}
         />
