37d509be4382148957b8575d64b9c0ac8fe85447
francocruz88 <francoplazafc@gmail.com>
2025-07-17 13:37:16 -0300

    boton solicitar vps

--- src/components/vps/VpsService.jsx
+++ src/components/vps/VpsService.jsx
@@ -46,7 +46,10 @@ export function VPSSection() {
             ))}
           </ul>
 
-          <button className="mt-8 bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition">
+          <button 
+            className="mt-8 bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition"
+            onClick={() => window.open("http://vps.onlygenius.es", "_blank")}
+          >
             Solicitar VPS →
           </button>
         </div>
